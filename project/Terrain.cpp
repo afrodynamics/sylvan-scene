d5b5002b951eda94de066c227e7a4a450339a48e
Dexter Friedman <drfriedm@ucsd.edu>
2014-12-10 21:16:56 -0800

    Trying recursive implementation, not really working

--- project/Terrain.cpp
+++ project/Terrain.cpp
@@ -6,16 +6,72 @@
 using namespace std;
 
 Terrain::Terrain() {
-	tesselX = tesselZ = 20;
+
+	// Seed the RNG
 	srand(198723);
-	generate(); // Generate vertices for the terrain
+
+	// Prepare the resolution of the heightmap
+	subdivisions = 6; // Be careful, exponential time going on here!
+	tesselX = tesselZ = pow(2, subdivisions) + 1; // This will be important later
+
+	// Do all the computation
+	generateHeightmap();
+	generateVerts(); // Generate vertices for the terrain
 };
 
 Terrain::~Terrain() {
 	// Nothing to destruct
 }
 
-void Terrain::generate() {
+/**
+ * Generates a heightmap by populating a square 2D array.
+ * Heightmap is in ROW-MAJOR, so the pixel (X,Y) is
+ *     heightmap[ Y * tesselX + X ]
+ */
+void Terrain::generateHeightmap() {
+	heightmap.resize( tesselX * tesselZ );
+	for ( int i = 0; i < tesselX - 1; ++i ) {
+		for ( int j = 0; j < tesselZ - 1; ++j ) {
+			heightmap[ j * tesselX + i ] = 0.0;
+		}
+	}
+	heightmap[ tesselZ / 2 * tesselX + tesselZ / 2 ] = diamondSquare(0,0,tesselX,tesselZ,.5);
+};
+
+/**
+ * An implementation of the diamond-square terrain generation algorithm.
+ * Described in great detail here: http://www.gameprogrammer.com/fractal.html
+ * A 2D application of the midpoint displacement algorithm
+ * @param  top       y coordinate of the current square
+ * @param  left      x coordinate of the current square
+ * @param  bottom    bottom y coordinate of the current square
+ * @param  right     right x coordinate of the current square
+ * @param  randRange positive double which clamps our random displacement
+ * @return           height value at the midpoint in the square
+ */
+double Terrain::diamondSquare(int top, int left, int bottom, int right, double randRange) {
+	int midX = (bottom - top) / 2;
+	int midY = (right - left) / 2;
+	int quartX = midX / 2;
+	int quartY = midY / 2;
+	double displacement = clamp(rand() % 256, -randRange, randRange);
+
+	// Recurse so long as we have space
+	if ( randRange > .125 ) {
+		// Top left square
+		heightmap[ quartX * tesselX + quartY ] = diamondSquare(top, left, midY, midX, randRange / 2.0);
+		// Top right square
+		heightmap[ (midX + quartX) * tesselX + quartY ] = diamondSquare(top, midX, midY, right, randRange / 2.0);
+		// Bottom left square
+		heightmap[ (quartX) * tesselX + quartY + midY ] = diamondSquare(midY, left, bottom, midX, randRange / 2.0);
+		// Bottom right square
+		heightmap[ (midX + quartX) * tesselX + quartY + midY ] = diamondSquare(midY, midX, bottom, right, randRange / 2.0);
+	}
+
+	return heightmap[ midY * tesselX + midX ] + displacement;
+}
+
+void Terrain::generateVerts() {
 	// Definitions
 	double minX, minZ;
 	double maxX, maxZ;
@@ -27,7 +83,7 @@ void Terrain::generate() {
 		for ( int z = 0; z < tesselZ; ++z ) {
 			double tx = (double)x / (double)tesselX;
 	 		double tz = (double)z / (double)tesselZ;
-			double yRand = 0.0;
+			double yRand = heightmap[ x * tesselZ + z ];
 			vertices.push_back( Vector3( lerp(tx, minX, maxX), yRand, lerp(tz, minZ, maxZ) ) );
 		}
 	}
@@ -58,6 +114,20 @@ void Terrain::render() {
 
 }
 
+// Linear interpolation
 double Terrain::lerp(double t, double p0, double p1) {
 	return ((1 - t) * p0 + t * p1);
+};
+
+// Keeps a number between the max/min values
+double Terrain::clamp(double val, double valMax, double valMin) {
+	if ( val < valMin ) {
+		return valMin;
+	}
+	else if ( val > valMax ) {
+		return valMax;
+	}
+	else {
+		return val;
+	}
 };
--- project/Terrain.h
+++ project/Terrain.h
@@ -5,11 +5,16 @@
 class Terrain : public Geode {
 private:
 	std::vector<Vector3> vertices;
+	std::vector<double> heightmap;
 	int tesselX, tesselZ;
+	int subdivisions;
 public:
 	Terrain();
 	~Terrain();
 	void render();
-	void generate();
+	void generateHeightmap();
+	void generateVerts();
+	double diamondSquare(int, int, int, int, double);
 	double lerp(double, double, double); // Lerp between two pts
+	double clamp(double, double, double); // Lerp between two pts
 };
